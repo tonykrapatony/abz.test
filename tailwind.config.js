23f6e244dece852bc794930b39ee8da273318ff5
tonykrapatony <tonykrapatony@gmail.com>
2024-01-04 16:33:54 +0200

    added test task

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,14 +20,29 @@ module.exports = {
     extend: {
       spacing: {
         16: '16px',
+        26: '26px',
+        104: '104px',
       },
       fontSize: {
         sm: '12px',
         md: '16px',
         lg: '40px',
       },
+      lineHeight: {
+        '14': '14px',
+        '26': '26px',
+        '40': '40px',
+      },
       fontFamily: {
         nunito: '"Nunito", sans-serif'
+      },
+      padding: {
+        '60px': '60px',
+        '13px': '13px',
+        '71px': '71px'
+      },
+      width: {
+        '104px': '104px',
       }
     },
     corePlugins: {
